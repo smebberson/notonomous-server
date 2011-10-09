cc4d9fc6bf61418b0d5367105dca254916863d5a
Scott Mebberson <scott@scottmebberson.com>
2011-10-09 23:07:19 +1030

    notonomous-server can now receive user information from notonomous

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var express = require('express');
 // define constants
 var PORT = 8889;
 
+// define variables
+var groups = [];
+
 // create the server
 var app = express.createServer();
 
@@ -14,6 +17,23 @@ app.listen(PORT);
 
 // init nowjs
 var everyone = nowjs.initialize(app);
+
+everyone.now.setUser = function (user) {
+	
+	// store the users username
+	this.now.user = user;
+	
+	// create a group specifically for the user, allows them to be logged into two locations (i.e. browser & iPad and sync content)
+	// define the group in the users now space
+	groups[this.now.user] = nowjs.getGroup(this.now.user);
+	
+	// add this user to the group
+	groups[this.now.user].addUser(this.user.clientId);
+	
+	// alert the client to the fact that we've set the user, we're now ready to save documents
+	this.now.userSet(this.user.clientId);
+	
+}
 
 // advise the server is running
 sys.puts('Notonomous-server is listening on http://localhost:' + PORT);
